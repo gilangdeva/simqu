11757e3cd37394fd0cafd5d38d047f81356c0d70
SindhuPurnamaSans <santoso3.sindhu@gmail.com>
2022-06-17 17:04:07 +0700

    220617 - Report

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -26,22 +26,131 @@ class ReportController extends Controller
     // Menampilkan list report
     public function ReportList(){
         // Get all data from database
-        $report = DB::select("SELECT * FROM vg_list_report");
 
-        $early_month       = date('Y-m-01', strtotime('+0 hours'));
-        $end_month = Carbon::createFromFormat('Y-m-d', $early_month)
-                        ->endOfMonth()
-                        ->format('Y-m-d');
 
-        $report = DB::table('vg_list_report')
-        ->where('tgl_inspeksi', '>=', $early_month)
-        ->where('tgl_inspeksi', '<=', $end_month)
-        ->get();
+        // $early_month       = date('Y-m-01', strtotime('+0 hours'));
+        // $end_month = Carbon::createFromFormat('Y-m-d', $early_month)
+        //                 ->endOfMonth()
+        //                 ->format('Y-m-d');
 
+        // $report_inline = DB::table('report_rekap_defect_inline')
+        // ->where('tgl_inspeksi', '>=', $early_month)
+        // ->where('tgl_inspeksi', '<=', $end_month)
+        // ->get();
+
+        $report_inl = DB::select("SELECT * FROM sp_report_defect_inline('Juni', '2022', '3', '".session()->get('id_user')."')");
+        $report_fnl = DB::select("SELECT * FROM sp_report_defect_final('Juni', '2022', '3', '".session()->get('id_user')."')");
+
+        $total_inl = DB::select("SELECT sum(total) FROM report_rekap_defect_inline WHERE id_user =".session()->get('id_user'));
+        $total_fnl = DB::select("SELECT sum(total) FROM report_rekap_defect_final WHERE id_user =".session()->get('id_user'));
+
+        $report_inline = DB::select("SELECT * FROM report_rekap_defect_inline WHERE id_user =".session()->get('id_user'));
+        $report_final = DB::select("SELECT * FROM report_rekap_defect_final WHERE id_user =".session()->get('id_user'));
+
+        $departemen = DB::select('SELECT id_departemen, nama_departemen FROM vg_list_departemen');
         return view('report.report-list',[
             'menu'   => 'report',
             'sub'    => '/report',
-            'report' => $report
+            'departemen'    => $departemen,
+            'report_inline' => $report_inline,
+            'report_final'  => $report_final,
+            'total_inl'     => $total_inl
         ]);
+    }
+
+
+    //Fungsi Filter List
+    public function FilterReportList(Request $request){
+
+        // Get value variable
+        $id_dept = $request->id_departemen;
+        $bulan = $request->bulan;
+
+        // Call Function / Stored Procedure
+        $report_inl = DB::select("SELECT * FROM sp_report_defect_inline('".$bulan."', '".date('Y', strtotime('+0 hours'))."', '".$id_dept."', '".session()->get('id_user')."')");
+        $report_fin = DB::select("SELECT * FROM sp_report_defect_final('".$bulan."', '".date('Y', strtotime('+0 hours'))."', '".$id_dept."', '".session()->get('id_user')."')");
+
+        //Get value total
+        $total_inl = DB::select("SELECT sum(total) as total_inline FROM report_rekap_defect_inline WHERE id_user =".session()->get('id_user'));
+        $total_fnl = DB::select("SELECT sum(total)as total_final FROM report_rekap_defect_final WHERE id_user =".session()->get('id_user'));
+
+        $total_inl = $total_inl[0]->total_inline;
+        $total_fnl = $total_fnl[0]->total_final;
+
+
+        // select data from table report
+        $report_inline = DB::select("SELECT * FROM report_rekap_defect_inline WHERE id_user =".session()->get('id_user'));
+        $report_final = DB::select("SELECT * FROM report_rekap_defect_final WHERE id_user =".session()->get('id_user'));
+
+        // Check total
+        // if(isset($total_inl)){
+        //     $total_inl = $total_inl;
+        // } else {
+        //     $total_inl = 0;
+        // }
+
+        // if(isset($total_fnl)){
+        //     $total_fnl = $total_fnl;
+        // } else {
+        //     $total_fnl = 0;
+        // }
+
+        // return back again list departemen
+        $departemen = DB::select('SELECT id_departemen, nama_departemen FROM vg_list_departemen');
+
+        $departemen = DB::select('SELECT id_departemen, nama_departemen FROM vg_list_departemen');
+        return view('report.report-list',[
+            'menu'   => 'report',
+            'sub'    => '/report',
+            'departemen'    => $departemen,
+            'report_inline' => $report_inline,
+            'report_final'  => $report_final,
+            'total_inl'     => $total_inl,
+            'total_fin'     => $total_fnl
+        ]);
+
+        // if ( || request()->bulan) {
+        //     $id_departemen    = $request->id_departemen;
+        //     $bulan              = $request->bulan;
+        //     $departemen = DB::select('SELECT id_departemen, nama_departemen FROM vg_list_departemen');
+
+        //         if ("id_departemen" == $request->id_departemen) {
+        //             $report_inl = DB::select("SELECT * FROM sp_report_defect_inline('".$bulan."', '".date('Y', strtotime('+0 hours'))."', '".$id_departemen."', '".session()->get('id_user')."')");
+        //             $report_inline = DB::table('report_rekap_defect_inline')
+        //                 ->where('id_departemen', '=', $id_departemen)
+        //                 ->get();
+        //         } else if ("bulan" == $bulan){
+        //             $report_inline = DB::table('report_rekap_defect_inline')
+        //                 ->where('bulan', '=', $bulan)
+        //                 ->get();
+        //         } else if ("id_departemen" == $id_departemen && "bulan" == $bulan){
+        //             $report_inline = DB::table('report_rekap_defect_inline')
+        //                 ->where('id_departemen', '=', $id_departemen)
+        //                 ->where('bulan', '=', $bulan)
+        //                 ->get();
+        //         } else {
+        //             $report_inline = DB::table('report_rekap_defect_inline')
+        //             ->get();
+        //         }
+
+        //     return view('report.report-list',
+        //     [
+        //         'report_inline' => $report_inline,
+        //         'departemen'    => $departemen,
+        //         'menu'          => 'report',
+        //         'sub'           => '/report'
+        //     ]);
+        // } else {
+        //     $departemen = DB::select('SELECT id_departemen, nama_departemen FROM vg_list_departemen');
+        //     $report_inline = DB::select("SELECT * FROM report_rekap_defect_inline");
+        //     return view('report.report-list',
+        //     [
+        //         'report_inline' => $report_inline,
+        //         'departemen'    => $departemen,
+        //         'menu'          => 'report',
+        //         'sub'           => '/report'
+        //     ]);
+        // }
+
     }
 }
--- resources/views/report/report-list.blade.php
+++ resources/views/report/report-list.blade.php
@@ -7,46 +7,220 @@
 <div class="container-fluid">
     <!-- row -->
     <br>
+    <div class="white-box">
+        <div class="row">
+            <form action="{{ route('report.filter') }}" id="report_data" class="form-horizontal" method="GET" enctype="multipart/form-data">
+                <div class="col-sm-5">
+                    <div class="col-sm-5">
+                        <select class="form-control select2" name="id_departemen" required>
+                            <option value="0">Pilih Departemen</option>
+                            @foreach ($departemen as $dept)
+                                <option value="{{ $dept->id_departemen }}">{{ $dept->nama_departemen }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+
+                <div class="col-sm-5">
+                    <div class="col-sm-5">
+                        <select class="form-control select-option" name="bulan" id="bulan">
+                            <option value="0">Pilih Bulan</option>
+                            <option value="Januari">JANUARI</option>
+                            <option value="Februari">FEBRUARI</option>
+                            <option value="Maret">MARET</option>
+                            <option value="April">APRIL</option>
+                            <option value="Mei">MEI</option>
+                            <option value="Juni">JUNI</option>
+                            <option value="Juli">JULI</option>
+                            <option value="Agustus">AGUSTUS</option>
+                            <option value="September">SEPTEMBER</option>
+                            <option value="Oktober">OKTOBER</option>
+                            <option value="November">NOVEMBER</option>
+                            <option value="Desember">DESEMBER</option>
+                        </select>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <button class="btn btn-primary waves-effect pull-right waves-light" type="submit">Submit</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
 
     <div class="row">
         <div class="col-md-12">
             <div class="white-box">
                 <div class="row">
                     <div class="col-sm-6 col-xs-12">
-                        <h3 class="box-title">LIST REPORT INSPEKSI</h3>
+                        <h3 class="box-title">REPORT INSPEKSI INLINE</h3>
                     </div>
                 </div>
 
-                <table id="demo-foo-pagination" class="table m-b-0 toggle-arrow-tiny inspeksi-list" data-page-size="20">
+                <table id="demo-foo" class="table m-b-0 toggle-arrow-tiny inspeksi-list">
                     <thead>
                         <tr>
                             <th data-toggle="true">No.</th>
-                            <th>Bagian</th>
                             <th>Periode</th>
-                            <th>Jml Total Inspeksi</th>
-                            <th>Jml Reject</th>
-                            <th>Jml Critical</th>
-                            <th>Jml Temuan Defect</th>
+                            <th>Tgl Awal</th>
+                            <th>Tgl Akhir</th>
+                            <th>Critical</th>
+                            <th>% Critical</th>
+                            <th>Major</th>
+                            <th>% Major</th>
+                            <th>Minor</th>
+                            <th>% Minor</th>
+                            <th>Total</th>
+                            <th>% Total</th>
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach($report as $r)
+                        @if(isset($report_inline[0]))
+                            @foreach($report_inline as $ri)
+                                <tr height="-10px;">
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>Minggu Ke{{ $ri->minggu_ke }}</td>
+                                    <td>{{ $ri->tgl_mulai_periode }}</td>
+                                    <td>{{ $ri->tgl_akhir_periode }}</td>
+                                    <td>{{ $ri->critical }}</td>
+                                    <td>{{ $ri->persen_critical }}%</td>
+                                    <td>{{ $ri->major }}</td>
+                                    <td>{{ $ri->persen_major }}%</td>
+                                    <td>{{ $ri->minor }}</td>
+                                    <td>{{ $ri->persen_minor }}%</td>
+                                    <td>{{ $ri->total }}</td>
+                                    <td>{{ ($ri->total/$total_inl)*100  }}%</td>
+                                </tr>
+                            @endforeach
+                        @else
+                            <tr>
+                                <td colspan="12">Tidak ada data untuk ditampilkan</td>
+                            </tr>
+                        @endif
+                    </tbody>
+
+                    <tfoot>
+                        <tr>
+                            <td colspan="12">
+                                <div class="text-right">
+                                </div>
+                            </td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="white-box">
+                <div class="row">
+                    <div class="col-sm-6 col-xs-12">
+                        <h3 class="box-title">REPORT INSPEKSI FINAL</h3>
+                    </div>
+                </div>
+
+                <table id="demo-foo" class="table m-b-0 toggle-arrow-tiny inspeksi-list">
+                    <thead>
+                        <tr>
+                            <th data-toggle="true">No.</th>
+                            <th>Periode</th>
+                            <th>Tgl Awal</th>
+                            <th>Tgl Akhir</th>
+                            <th>Pass</th>
+                            <th>% Pass</th>
+                            <th>Reject</th>
+                            <th>% Reject</th>
+                            <th>Total</th>
+                            <th>% Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @if(isset($report_final[0]))
+                            @foreach($report_final as $fin)
+                                <tr height="-10px;">
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>Minggu Ke{{ $fin->minggu_ke }}</td>
+                                    <td>{{ $fin->tgl_mulai_periode }}</td>
+                                    <td>{{ $fin->tgl_akhir_periode }}</td>
+                                    <td>{{ $fin->pass }}</td>
+                                    <td>{{ $fin->persen_pass }}%</td>
+                                    <td>{{ $fin->reject }}</td>
+                                    <td>{{ $fin->persen_reject }}%</td>
+                                    <td>{{ $fin->total }}</td>
+                                    <td>{{ ($fin->total/$total_fnl)*100 }}%</td>
+                                </tr>
+                            @endforeach
+                        @else
+                            <tr>
+                                <td colspan="11">Tidak ada data untuk ditampilkan</td>
+                            </tr>
+                        @endif
+                    </tbody>
+
+                    <tfoot>
+                        <tr>
+                            <td colspan="12">
+                            </td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="white-box">
+                <div class="row">
+                    <div class="col-sm-6 col-xs-12">
+                        <h3 class="box-title">REKAP</h3>
+                    </div>
+                </div>
+
+                <table id="demo-foo" class="table m-b-0 toggle-arrow-tiny inspeksi-list">
+                    <thead>
+                        <tr>
+                            <th data-toggle="true">No.</th>
+                            <th>Periode</th>
+                            <th>Tgl Awal</th>
+                            <th>Tgl Akhir</th>
+                            <th>Critical</th>
+                            <th>Persentase Critical</th>
+                            <th>Major</th>
+                            <th>Persentase Major</th>
+                            <th>Minor</th>
+                            <th>Persentase Minor</th>
+                            <th>Total</th>
+                            <th>Total Sampling</th>
+                            <th>Persentase Sampling</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($report_inline as $ri)
                             <tr height="-10px;">
                                 <td>{{ $loop->iteration }}</td>
-                                <td>{{ $r->nama_departemen }}</td>
-                                <td>{{ $r->nama_departemen }}</td>
-                                <td>{{ $r->qty_defect }}</td>
-                                <td>{{ $r->qty_defect }}</td>
+                                <td>Minggu Ke{{ $ri->minggu_ke }}</td>
+                                <td>{{ $ri->tgl_mulai_periode }}</td>
+                                <td>{{ $ri->tgl_akhir_periode }}</td>
+                                <td>{{ $ri->critical }}</td>
+                                <td>{{ $ri->persen_critical }}%</td>
+                                <td>{{ $ri->major }}</td>
+                                <td>{{ $ri->persen_major }}%</td>
+                                <td>{{ $ri->minor }}</td>
+                                <td>{{ $ri->persen_minor }}%</td>
+                                <td>{{ $ri->total }}</td>
+                                {{-- <td>{{ $ri->total_sampling }}</td> --}}
+                                {{-- <td>{{ $ri->persen_sampling }}</td> --}}
                             </tr>
                         @endforeach
                     </tbody>
 
                     <tfoot>
                         <tr>
-                            <td colspan="8">
-                                <div class="text-right">
-                                    <ul class="pagination pagination-split m-t-30"> </ul>
-                                </div>
+                            <td colspan="15">
                             </td>
                         </tr>
                     </tfoot>
