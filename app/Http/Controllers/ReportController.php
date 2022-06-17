c4f02d5f1136c811334c5f29db5987f6e080141c
Gilang Deva <66029596+gilangdeva@users.noreply.github.com>
2022-06-17 13:50:51 +0700

    220617 - Report inspeksi inline

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -26,22 +26,32 @@ class ReportController extends Controller
     // Menampilkan list report
     public function ReportList(){
         // Get all data from database
-        $report = DB::select("SELECT * FROM vg_list_report");
 
-        $early_month       = date('Y-m-01', strtotime('+0 hours'));
-        $end_month = Carbon::createFromFormat('Y-m-d', $early_month)
-                        ->endOfMonth()
-                        ->format('Y-m-d');
 
-        $report = DB::table('vg_list_report')
-        ->where('tgl_inspeksi', '>=', $early_month)
-        ->where('tgl_inspeksi', '<=', $end_month)
-        ->get();
+        // $early_month       = date('Y-m-01', strtotime('+0 hours'));
+        // $end_month = Carbon::createFromFormat('Y-m-d', $early_month)
+        //                 ->endOfMonth()
+        //                 ->format('Y-m-d');
+
+        // $report_inline = DB::table('report_rekap_defect_inline')
+        // ->where('tgl_inspeksi', '>=', $early_month)
+        // ->where('tgl_inspeksi', '<=', $end_month)
+        // ->get();
+
+        $report_inl = DB::select("SELECT * FROM sp_report_defect_inline('Juni', '2022', '3', '".session()->get('id_user')."')");
+        $report_inl = $report_inl[0]->sp_report_defect_inline;
+
+        $total_inl = DB::select("SELECT sum(total) FROM report_rekap_defect_inline WHERE id_user =".session()->get('id_user'));
+        $total_inl = $total_inl[0]->sum;
+
+        $report_inline = DB::select("SELECT * FROM report_rekap_defect_inline WHERE id_user =".session()->get('id_user'));
 
         return view('report.report-list',[
             'menu'   => 'report',
             'sub'    => '/report',
-            'report' => $report
+            'report_inline' => $report_inline,
+            'report_inl'    => $report_inl,
+            'total_inl'     => $total_inl
         ]);
     }
 }
--- resources/views/report/report-list.blade.php
+++ resources/views/report/report-list.blade.php
@@ -7,46 +7,210 @@
 <div class="container-fluid">
     <!-- row -->
     <br>
+    <div class="white-box">
+        <div class="row">
+            <form action="{{ route('inline.filter') }}" id="inline_data" class="form-horizontal" method="GET" enctype="multipart/form-data">
+                <div class="col-sm-5">
+                    <div class="col-sm-5">
+                        <select class="form-control select-option" name="nama_departemen" id="nama_departemen">
+                            <option value="0">Pilih Bagian :</option>
+                            <option value="HANDMADE">HANDMADE</option>
+                            <option value="MANUAL">MANUAL</option>
+                            <option value="SOFTCOVER">SOFTCOVER</option>
+                            <option value="HARDCOVER">HARDCOVER</option>
+                            <option value="MNLPACKAGING">MNL PACKAGING</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="col-sm-5">
+                    <div class="col-sm-5">
+                        <select class="form-control select-option" name="bulan" id="bulan">
+                            <option value="0">Pilih Bulan :</option>
+                            <option value="JANUARI">JANUARI</option>
+                            <option value="FEBRUARI">FEBRUARI</option>
+                            <option value="MARET">MARET</option>
+                            <option value="APRIL">APRIL</option>
+                            <option value="MEI">MEI</option>
+                            <option value="JUNI">JUNI</option>
+                            <option value="JULI">JULI</option>
+                            <option value="AGUSTUS">AGUSTUS</option>
+                            <option value="SEPTEMBER">SEPTEMBER</option>
+                            <option value="OKTOBER">OKTOBER</option>
+                            <option value="NOVEMBER">NOVEMBER</option>
+                            <option value="DESEMBER">DESEMBER</option>
+                        </select>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <button class="btn btn-primary waves-effect pull-right waves-light" type="submit">Submit</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
 
     <div class="row">
         <div class="col-md-12">
             <div class="white-box">
                 <div class="row">
                     <div class="col-sm-6 col-xs-12">
-                        <h3 class="box-title">LIST REPORT INSPEKSI</h3>
+                        <h3 class="box-title">REPORT INSPEKSI INLINE</h3>
                     </div>
                 </div>
 
-                <table id="demo-foo-pagination" class="table m-b-0 toggle-arrow-tiny inspeksi-list" data-page-size="20">
+                <table id="demo-foo" class="table m-b-0 toggle-arrow-tiny inspeksi-list">
                     <thead>
                         <tr>
                             <th data-toggle="true">No.</th>
-                            <th>Bagian</th>
                             <th>Periode</th>
-                            <th>Jml Total Inspeksi</th>
-                            <th>Jml Reject</th>
-                            <th>Jml Critical</th>
-                            <th>Jml Temuan Defect</th>
+                            <th>Tgl Awal</th>
+                            <th>Tgl Akhir</th>
+                            <th>Critical</th>
+                            <th>Persentase Critical</th>
+                            <th>Major</th>
+                            <th>Persentase Major</th>
+                            <th>Minor</th>
+                            <th>Persentase Minor</th>
+                            <th>Total</th>
+                            <th>Persentase Total</th>
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach($report as $r)
+                        @foreach($report_inline as $ri)
                             <tr height="-10px;">
                                 <td>{{ $loop->iteration }}</td>
-                                <td>{{ $r->nama_departemen }}</td>
-                                <td>{{ $r->nama_departemen }}</td>
-                                <td>{{ $r->qty_defect }}</td>
-                                <td>{{ $r->qty_defect }}</td>
+                                <td>Minggu Ke{{ $ri->minggu_ke }}</td>
+                                <td>{{ $ri->tgl_mulai_periode }}</td>
+                                <td>{{ $ri->tgl_akhir_periode }}</td>
+                                <td>{{ $ri->critical }}</td>
+                                <td>{{ $ri->persen_critical }}%</td>
+                                <td>{{ $ri->major }}</td>
+                                <td>{{ $ri->persen_major }}%</td>
+                                <td>{{ $ri->minor }}</td>
+                                <td>{{ $ri->persen_minor }}%</td>
+                                <td>{{ $ri->total }}</td>
+                                <td>{{ $ri->total/($total_inl)*100 }}</td>
                             </tr>
                         @endforeach
                     </tbody>
 
                     <tfoot>
                         <tr>
-                            <td colspan="8">
+                            <td colspan="12">
                                 <div class="text-right">
-                                    <ul class="pagination pagination-split m-t-30"> </ul>
                                 </div>
+                            </td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="white-box">
+                <div class="row">
+                    <div class="col-sm-6 col-xs-12">
+                        <h3 class="box-title">REPORT INSPEKSI FINAL</h3>
+                    </div>
+                </div>
+
+                <table id="demo-foo" class="table m-b-0 toggle-arrow-tiny inspeksi-list">
+                    <thead>
+                        <tr>
+                            <th data-toggle="true">No.</th>
+                            <th>Periode</th>
+                            <th>Tgl Awal</th>
+                            <th>Tgl Akhir</th>
+                            <th>Pass</th>
+                            <th>Persentase Pass</th>
+                            <th>Reject</th>
+                            <th>Persentase Reject</th>
+                            <th>Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($report_inline as $ri)
+                            <tr height="-10px;">
+                                <td>{{ $loop->iteration }}</td>
+                                <td>Minggu Ke{{ $ri->minggu_ke }}</td>
+                                <td>{{ $ri->tgl_mulai_periode }}</td>
+                                <td>{{ $ri->tgl_akhir_periode }}</td>
+                                <td>{{ $ri->critical }}</td>
+                                <td>{{ $ri->persen_critical }}%</td>
+                                <td>{{ $ri->major }}</td>
+                                <td>{{ $ri->persen_major }}%</td>
+                                <td>{{ $ri->minor }}</td>
+                                <td>{{ $ri->persen_minor }}%</td>
+                                <td>{{ $ri->total }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+
+                    <tfoot>
+                        <tr>
+                            <td colspan="12">
+                            </td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="white-box">
+                <div class="row">
+                    <div class="col-sm-6 col-xs-12">
+                        <h3 class="box-title">REKAP</h3>
+                    </div>
+                </div>
+
+                <table id="demo-foo" class="table m-b-0 toggle-arrow-tiny inspeksi-list">
+                    <thead>
+                        <tr>
+                            <th data-toggle="true">No.</th>
+                            <th>Periode</th>
+                            <th>Tgl Awal</th>
+                            <th>Tgl Akhir</th>
+                            <th>Critical</th>
+                            <th>Persentase Critical</th>
+                            <th>Major</th>
+                            <th>Persentase Major</th>
+                            <th>Minor</th>
+                            <th>Persentase Minor</th>
+                            <th>Total</th>
+                            <th>Total Sampling</th>
+                            <th>Persentase Sampling</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($report_inline as $ri)
+                            <tr height="-10px;">
+                                <td>{{ $loop->iteration }}</td>
+                                <td>Minggu Ke{{ $ri->minggu_ke }}</td>
+                                <td>{{ $ri->tgl_mulai_periode }}</td>
+                                <td>{{ $ri->tgl_akhir_periode }}</td>
+                                <td>{{ $ri->critical }}</td>
+                                <td>{{ $ri->persen_critical }}%</td>
+                                <td>{{ $ri->major }}</td>
+                                <td>{{ $ri->persen_major }}%</td>
+                                <td>{{ $ri->minor }}</td>
+                                <td>{{ $ri->persen_minor }}%</td>
+                                <td>{{ $ri->total }}</td>
+                                {{-- <td>{{ $ri->total_sampling }}</td> --}}
+                                {{-- <td>{{ $ri->persen_sampling }}</td> --}}
+                            </tr>
+                        @endforeach
+                    </tbody>
+
+                    <tfoot>
+                        <tr>
+                            <td colspan="15">
                             </td>
                         </tr>
                     </tfoot>
