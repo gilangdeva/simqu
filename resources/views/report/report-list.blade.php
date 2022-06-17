a7960e3bec8ccf12224f52f3680441c462edaf55
Gilang Deva <66029596+gilangdeva@users.noreply.github.com>
2022-06-17 16:57:15 +0700

    220617 - Report Lengkap

--- resources/views/report/report-list.blade.php
+++ resources/views/report/report-list.blade.php
@@ -9,16 +9,14 @@
     <br>
     <div class="white-box">
         <div class="row">
-            <form action="{{ route('inline.filter') }}" id="inline_data" class="form-horizontal" method="GET" enctype="multipart/form-data">
+            <form action="{{ route('report.filter') }}" id="report_data" class="form-horizontal" method="GET" enctype="multipart/form-data">
                 <div class="col-sm-5">
                     <div class="col-sm-5">
-                        <select class="form-control select-option" name="nama_departemen" id="nama_departemen">
-                            <option value="0">Pilih Bagian :</option>
-                            <option value="HANDMADE">HANDMADE</option>
-                            <option value="MANUAL">MANUAL</option>
-                            <option value="SOFTCOVER">SOFTCOVER</option>
-                            <option value="HARDCOVER">HARDCOVER</option>
-                            <option value="MNLPACKAGING">MNL PACKAGING</option>
+                        <select class="form-control select2" name="id_departemen" required>
+                            <option value="0">Pilih Departemen</option>
+                            @foreach ($departemen as $dept)
+                                <option value="{{ $dept->id_departemen }}">{{ $dept->nama_departemen }}</option>
+                            @endforeach
                         </select>
                     </div>
                 </div>
@@ -26,19 +24,19 @@
                 <div class="col-sm-5">
                     <div class="col-sm-5">
                         <select class="form-control select-option" name="bulan" id="bulan">
-                            <option value="0">Pilih Bulan :</option>
-                            <option value="JANUARI">JANUARI</option>
-                            <option value="FEBRUARI">FEBRUARI</option>
-                            <option value="MARET">MARET</option>
-                            <option value="APRIL">APRIL</option>
-                            <option value="MEI">MEI</option>
-                            <option value="JUNI">JUNI</option>
-                            <option value="JULI">JULI</option>
-                            <option value="AGUSTUS">AGUSTUS</option>
-                            <option value="SEPTEMBER">SEPTEMBER</option>
-                            <option value="OKTOBER">OKTOBER</option>
-                            <option value="NOVEMBER">NOVEMBER</option>
-                            <option value="DESEMBER">DESEMBER</option>
+                            <option value="0">Pilih Bulan</option>
+                            <option value="Januari">JANUARI</option>
+                            <option value="Februari">FEBRUARI</option>
+                            <option value="Maret">MARET</option>
+                            <option value="April">APRIL</option>
+                            <option value="Mei">MEI</option>
+                            <option value="Juni">JUNI</option>
+                            <option value="Juli">JULI</option>
+                            <option value="Agustus">AGUSTUS</option>
+                            <option value="September">SEPTEMBER</option>
+                            <option value="Oktober">OKTOBER</option>
+                            <option value="November">NOVEMBER</option>
+                            <option value="Desember">DESEMBER</option>
                         </select>
                     </div>
 
@@ -68,32 +66,38 @@
                             <th>Tgl Awal</th>
                             <th>Tgl Akhir</th>
                             <th>Critical</th>
-                            <th>Persentase Critical</th>
+                            <th>% Critical</th>
                             <th>Major</th>
-                            <th>Persentase Major</th>
+                            <th>% Major</th>
                             <th>Minor</th>
-                            <th>Persentase Minor</th>
+                            <th>% Minor</th>
                             <th>Total</th>
-                            <th>Persentase Total</th>
+                            <th>% Total</th>
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach($report_inline as $ri)
-                            <tr height="-10px;">
-                                <td>{{ $loop->iteration }}</td>
-                                <td>Minggu Ke{{ $ri->minggu_ke }}</td>
-                                <td>{{ $ri->tgl_mulai_periode }}</td>
-                                <td>{{ $ri->tgl_akhir_periode }}</td>
-                                <td>{{ $ri->critical }}</td>
-                                <td>{{ $ri->persen_critical }}%</td>
-                                <td>{{ $ri->major }}</td>
-                                <td>{{ $ri->persen_major }}%</td>
-                                <td>{{ $ri->minor }}</td>
-                                <td>{{ $ri->persen_minor }}%</td>
-                                <td>{{ $ri->total }}</td>
-                                <td>{{ $ri->total/($total_inl)*100 }}</td>
+                        @if(isset($report_inline[0]))
+                            @foreach($report_inline as $ri)
+                                <tr height="-10px;">
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>Minggu Ke{{ $ri->minggu_ke }}</td>
+                                    <td>{{ $ri->tgl_mulai_periode }}</td>
+                                    <td>{{ $ri->tgl_akhir_periode }}</td>
+                                    <td>{{ $ri->critical }}</td>
+                                    <td>{{ $ri->persen_critical }}%</td>
+                                    <td>{{ $ri->major }}</td>
+                                    <td>{{ $ri->persen_major }}%</td>
+                                    <td>{{ $ri->minor }}</td>
+                                    <td>{{ $ri->persen_minor }}%</td>
+                                    <td>{{ $ri->total }}</td>
+                                    <td>{{ ($ri->total/$total_inl)*100  }}%</td>
+                                </tr>
+                            @endforeach
+                        @else
+                            <tr>
+                                <td colspan="12">Tidak ada data untuk ditampilkan</td>
                             </tr>
-                        @endforeach
+                        @endif
                     </tbody>
 
                     <tfoot>
@@ -126,28 +130,34 @@
                             <th>Tgl Awal</th>
                             <th>Tgl Akhir</th>
                             <th>Pass</th>
-                            <th>Persentase Pass</th>
+                            <th>% Pass</th>
                             <th>Reject</th>
-                            <th>Persentase Reject</th>
+                            <th>% Reject</th>
                             <th>Total</th>
+                            <th>% Total</th>
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach($report_inline as $ri)
-                            <tr height="-10px;">
-                                <td>{{ $loop->iteration }}</td>
-                                <td>Minggu Ke{{ $ri->minggu_ke }}</td>
-                                <td>{{ $ri->tgl_mulai_periode }}</td>
-                                <td>{{ $ri->tgl_akhir_periode }}</td>
-                                <td>{{ $ri->critical }}</td>
-                                <td>{{ $ri->persen_critical }}%</td>
-                                <td>{{ $ri->major }}</td>
-                                <td>{{ $ri->persen_major }}%</td>
-                                <td>{{ $ri->minor }}</td>
-                                <td>{{ $ri->persen_minor }}%</td>
-                                <td>{{ $ri->total }}</td>
+                        @if(isset($report_final[0]))
+                            @foreach($report_final as $fin)
+                                <tr height="-10px;">
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>Minggu Ke{{ $fin->minggu_ke }}</td>
+                                    <td>{{ $fin->tgl_mulai_periode }}</td>
+                                    <td>{{ $fin->tgl_akhir_periode }}</td>
+                                    <td>{{ $fin->pass }}</td>
+                                    <td>{{ $fin->persen_pass }}%</td>
+                                    <td>{{ $fin->reject }}</td>
+                                    <td>{{ $fin->persen_reject }}%</td>
+                                    <td>{{ $fin->total }}</td>
+                                    <td>{{ ($fin->total/$total_fnl)*100 }}%</td>
+                                </tr>
+                            @endforeach
+                        @else
+                            <tr>
+                                <td colspan="11">Tidak ada data untuk ditampilkan</td>
                             </tr>
-                        @endforeach
+                        @endif
                     </tbody>
 
                     <tfoot>
@@ -166,7 +176,7 @@
             <div class="white-box">
                 <div class="row">
                     <div class="col-sm-6 col-xs-12">
-                        <h3 class="box-title">REKAP</h3>
+                        <h3 class="box-title">REPORT KRITERIA</h3>
                     </div>
                 </div>
 
@@ -178,34 +188,39 @@
                             <th>Tgl Awal</th>
                             <th>Tgl Akhir</th>
                             <th>Critical</th>
-                            <th>Persentase Critical</th>
+                            <th>% Critical</th>
                             <th>Major</th>
-                            <th>Persentase Major</th>
+                            <th>% Major</th>
                             <th>Minor</th>
-                            <th>Persentase Minor</th>
+                            <th>% Minor</th>
                             <th>Total</th>
                             <th>Total Sampling</th>
-                            <th>Persentase Sampling</th>
+                            <th>% Sampling</th>
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach($report_inline as $ri)
-                            <tr height="-10px;">
-                                <td>{{ $loop->iteration }}</td>
-                                <td>Minggu Ke{{ $ri->minggu_ke }}</td>
-                                <td>{{ $ri->tgl_mulai_periode }}</td>
-                                <td>{{ $ri->tgl_akhir_periode }}</td>
-                                <td>{{ $ri->critical }}</td>
-                                <td>{{ $ri->persen_critical }}%</td>
-                                <td>{{ $ri->major }}</td>
-                                <td>{{ $ri->persen_major }}%</td>
-                                <td>{{ $ri->minor }}</td>
-                                <td>{{ $ri->persen_minor }}%</td>
-                                <td>{{ $ri->total }}</td>
-                                {{-- <td>{{ $ri->total_sampling }}</td> --}}
-                                {{-- <td>{{ $ri->persen_sampling }}</td> --}}
+                        @if(isset($report_kriteria[0]))
+                            @foreach($report_kriteria as $krt)
+                                <tr height="-10px;">
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>Minggu Ke{{ $krt->minggu_ke }}</td>
+                                    <td>{{ $krt->tgl_mulai_periode }}</td>
+                                    <td>{{ $krt->tgl_akhir_periode }}</td>
+                                    <td>{{ $krt->critical }}</td>
+                                    <td>{{ $krt->persen_critical }}%</td>
+                                    <td>{{ $krt->major }}</td>
+                                    <td>{{ $krt->persen_major }}%</td>
+                                    <td>{{ $krt->minor }}</td>
+                                    <td>{{ $krt->persen_minor }}%</td>
+                                    <td>{{ $krt->qty_riil }}</td>
+                                    <td>{{ ($krt->total/$total_krt)*100 }}%</td>
+                                </tr>
+                            @endforeach
+                        @else
+                            <tr>
+                                <td colspan="12">Tidak ada data untuk ditampilkan</td>
                             </tr>
-                        @endforeach
+                        @endif
                     </tbody>
 
                     <tfoot>
